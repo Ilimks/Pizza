09627b128d02eae5f8dbc81e3a72e654e7af742a
Ilim <ilimk9731@gmail.com>
2025-02-02 19:01:02 +0600

    header & footer

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -1,11 +1,103 @@
 import React from 'react';
 import './Footer.css'
+import footerLogo from './FooterIMG/FooterLogo.svg' 
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className='footer'>
+            <div className="container">
+                <div className="footer__box">
+
+                    <div className="footer__left1">
+                        <div className="footer__left1__name">
+                            <img className='footer__left1__name__img' src={footerLogo} alt="" />
+                            <p className='footer__left1__name__text'>Куда пицца</p>
+                        </div>
+                        <p className='footer__left1__bottom'>© Copyright 2021 — Куда Пицца</p>
+                    </div>
+
+                    <div className="footer__left2">
+                        <h3 className='footer__left2__name'>Куда пицца</h3>
+                        <ul className='footer__left2__nav'>
+                            <li className='f__left2__nav__list'>О компании</li>
+                            <li className='f__left2__nav__list'>Пользовательское соглашение</li>
+                            <li className='f__left2__nav__list'>Условия гарантии</li>
+                        </ul>
+                    </div>
+
+                    <div className="footer__center">
+                        <h3 className='footer__center__name'>Помощь</h3>
+                        <ul className='footer__center__nav'>
+                            <li className='f__center__nav__list'>Ресторан</li>
+                            <li className='f__center__nav__list'>Контакты</li>
+                            <li className='f__center__nav__list'>Поддержка</li>
+                            <li className='f__center__nav__list'>Отследить заказ</li>
+                        </ul>
+                    </div>
+
+                    <div className="footer__right">
+                        <h3 className='footer__right__name'>Контакты</h3>
+                        <div className="footer__right__adress">
+                            <p className='f__right__adress__list'>+7 (926) 223-10-11</p>
+                            <p className='f__right__adress__list'>Москва, ул. Юных Ленинцев, д.99</p>
+                        </div>
+                        <div className="footer__right__net">
+                            <p className='f__right__net'>Facebook</p>
+                            <p className='f__right__net'>Instagram</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="footer__box__mobile">
+
+                    <div className="footer__mobile__name">
+                        <img className='f__mobile__name__img' src={footerLogo} alt="" />
+                        <p className='f__mobile__name__text'>Куда пицца</p>
+                    </div>
+
+                    <div className="footer__box__mobile__up1">
+                        <h3 className='f__box__mobile__name'>Куда пицца</h3>
+                        <ul className='f__box__mobile__nav'>
+                            <li className='f__box__mobile__nav__list'>О компании</li>
+                            <li className='f__box__mobile__nav__list'>Пользовательское соглашение</li>
+                            <li className='f__box__mobile__nav__list'>Условия гарантии</li>
+                        </ul>
+                    </div>
+
+                    <div className="footer__box__mobile__up2">
+                        <h3 className='f__box__mobile__name'>Куда пицца</h3>
+                        <ul className='f__box__mobile__nav'>
+                            <li className='f__box__mobile__nav__list'>О компании</li>
+                            <li className='f__box__mobile__nav__list'>Пользовательское соглашение</li>
+                            <li className='f__box__mobile__nav__list'>Условия гарантии</li>
+                        </ul>
+                    </div>
+
+                    <div className="footer__box__mobile__center">
+                        <h3 className='f__box__mobile__name'>Помощь</h3>
+                        <ul className='f__box__mobile__nav'>
+                            <li className='f__box__mobile__nav__list'>Ресторан</li>
+                            <li className='f__box__mobile__nav__list'>Контакты</li>
+                            <li className='f__box__mobile__nav__list'>Поддержка</li>
+                            <li className='f__box__mobile__nav__list'>Отследить заказ</li>
+                        </ul>
+                    </div>
+
+                    <div className="footer__box__mobile__right">
+                        <h3 className='f__box__mobile__name'>Контакты</h3>
+                        <div className="f__box__mobile__adress">
+                            <p className='f__box__mobile__adress__list'>+7 (926) 223-10-11</p>
+                            <p className='f__box__mobile__adress__list'>Москва, ул. Юных Ленинцев, д.99</p>
+                        </div>
+                        <div className="f__box__mobile__net">
+                            <p className='f__box__mobile__net__list'>Facebook</p>
+                            <p className='f__box__mobile__net__list'>Instagram</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
     );
 };
 
--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
 
     useEffect(()=>{
         const headScroll = throttle(() => {
-            setScrolled(window.scrollY > 30);
+            setScrolled(window.scrollY > 90);
         }, 200);
         window.addEventListener('scroll', headScroll);
         return () => window.removeEventListener('scroll', headScroll);
@@ -24,93 +24,88 @@ const Header = () => {
     return (
         <header className='header'>
             <div className="header__container">
-                
+                <motion.div
+                className='header__noScrolled'
+                initial={{ opacity: 1 }}
+                animate={{ opacity: scrolled ? 0 : 1, y: scrolled ? -20 : 0 }}
+                transition={{ duration: 0.3 }}>
+                    <div className="header__top">
+                        <div className="header__top__left">
+                            <div className="header__top__left__city">
+                                <img className='header__top__left__city__img' src={headerLocation} alt="city" />
+                                <p className='header__top__left__city__text'>Москва</p>
+                            </div>
+                            <p className='header__top__left__adress'>Проверить адрес</p>
+                            <p className='header__top__left__time'>Среднее время доставки*: <span>00:00:00</span></p>
+                            <p className='header__top__left__time__mobile'>00:00:00</p>
+                        </div>
+                        <div className="header__top__right">
+                            <p className='header__top__right__timeWork'>Время работы: с 11:00 до 23:00</p>
+                            <div className="header__top__right__account">
+                                <img className='header__top__right__account__img' src={headerIconAccount} alt="account" />
+                                <p className='header__top__right__account__text'>Войти в аккаунт</p>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div className="header__line"></div>
+    
+                    <div className="header__bottom">
+                        <div className="header__bottom__pizza">
+                            <img className='header__bottom__pizza__img' src={headerPizza} alt="pizza" />
+                            <p className='header__bottom__pizza__text'>Куда пицца</p>
+                        </div>
+                        <button className='header__bottom__btn'>0 ₽</button>
+                        <label className='burger-3'>
+                            <input type="checkbox"/>
+                            <svg viewBox="0 0 32 32">
+                              <path
+                                className="line line-top-bottom"
+                                d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+                              ></path>
+                              <path className="line" d="M7 16 27 16"></path>
+                            </svg>
+                        </label>
+                    </div>
+                </motion.div>
+
+                {scrolled && (
+                <motion.div 
+                className="header__scrolled"
+                initial={{ y: -100, opacity: 0 }}
+                animate={{ y: scrolled ? 0 : -100, opacity: scrolled ? 1 : 0 }}
+                transition={{ duration: 0.3 }}>
+                    <div className="header__bottom__scrolled">
+
+                        <div className="header__bottom__scrolled__left">
+                            <div className="header__bottom__scrolled__left__pizza">
+                                <img className='header__bottom__scrolled__left__pizza__img' src={headerPizza} alt="pizza" />
+                                <p className='header__bottom__scrolled__left__pizza__text'>Куда пицца</p>
+                            </div>
+                            <nav className='header__nav'>
+                                <ul className='header__nav__menu'>
+                                    <li className='h__nav__menu__item'>Акции</li>
+                                    <li className='h__nav__menu__item'>Пицца</li>
+                                    <li className='h__nav__menu__item'>Суши</li>
+                                    <li className='h__nav__menu__item'>Напитки</li>
+                                    <li className='h__nav__menu__item'>Закуски</li>
+                                    <li className='h__nav__menu__item'>Комбо</li>
+                                    <li className='h__nav__menu__item'>Десерты</li>
+                                    <li className='h__nav__menu__item'>Соусы</li>
+                                    <li className='h__nav__menu__item'>Другое</li>
+                                </ul>
+                            </nav>
+                        </div>
+
+                        <button className='header__bottom__scrolled__left__btn'>0 ₽</button>
+
+                    </div>
+                </motion.div>
+                )}
+
             </div>
         </header>
         
-        // <header className="header">
-        //     {!scrolled && (
-        //     <motion.div className="header__box"
-        //     initial={{ opacity: 1 }}
-        //     animate={{ opacity: scrolled ? 0 : 1, y: scrolled ? -20 : 0 }}
-        //     transition={{ duration: 0.3 }}>
-        //         <div className="container">
-        //             <div className="header__box__up">
-        //                 <div className="header__box__up__left">
-        //                     <div className="header__box__up__left__moscow">
-        //                         <img className='header__box__up__left__moscow__location' src={headerLocation} alt="Локация" />
-        //                         <p className='header__box__up__left__moscow__text'>Москва</p>
-        //                         <img className='header__box__up__left__moscow__arrow' src={headerArrowDown} alt="Стрелка вниз" />
-        //                     </div>
-        //                     <p className='header__box__up__left__adress'>Проверить адрес</p>
-        //                     <p className='header__box__up__left__time'>Среднее время доставки*: <span>00:00:00</span></p>
-        //                 </div>
-        //                 <div className="header__box__up__right">
-        //                     <p className='header__box__up__right__timeWork'>Время работы: с 11:00 до 23:00</p>
-        //                     <div className="header__box__up__right__account">
-        //                         <img className='header__box__up__right__account__icon' src={headerIconAccount} alt="Иконка аккаунт"/>
-        //                         <p className='header__box__up__right__account__text'>Войти в аккаунт</p>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
-
-        //         <div className="header__container">
-        //             <div className="header__box__center__line"></div>
-        //         </div>
-                
-        //         <div className="container">
-        //             <div className="header__box__bottom">
-
-        //                 <div className="header__box__bottom__left">
-        //                     <img className='header__box__bottom__left__pizza' src={headerPizza} alt="Иконка пицца" />
-        //                     <p className='header__box__bottom__left__text'>Куда пицца</p>
-        //                 </div>
-                        
-        //                 <div className="header__box__bottom__btn">
-        //                     <img className='header__box__bottom__btn__bag' src={headerShopBag} alt="Корзина" />
-        //                     <p className='header__box__bottom__btn__r'>0 ₽</p>
-        //                 </div>
-
-        //             </div>
-        //         </div>
-        //     </motion.div>
-        //     )}
-
-        //     {scrolled && (
-        //         <div className="container">
-        //             <motion.div className="header__scrolled"
-        //             initial={{ y: -100, opacity: 0 }}
-        //             animate={{ y: scrolled ? 0 : -100, opacity: scrolled ? 1 : 0 }}
-        //             transition={{ duration: 0.4 }}>
-        //                 <div className="header__scrolled__left">
-        //                     <div className="header__box__bottom__left">
-        //                         <img className='header__box__bottom__left__pizza' src={headerPizza} alt="Иконка пицца" />
-        //                         <p className='header__box__bottom__left__text'>Куда пицца</p>
-        //                     </div>    
-        //                     <nav className='header__box__nav'>
-        //                         <ul className='header__box__nav__ul'>
-        //                             <li className='header__box__nav__li'>Акции</li>
-        //                             <li className='header__box__nav__li'>Пицца</li>
-        //                             <li className='header__box__nav__li'>Суши</li>
-        //                             <li className='header__box__nav__li'>Напитки</li>
-        //                             <li className='header__box__nav__li'>Закуски</li>
-        //                             <li className='header__box__nav__li'>Комбо</li>
-        //                             <li className='header__box__nav__li'>Десерты</li>
-        //                             <li className='header__box__nav__li'>Соусы</li>
-        //                             <li className='header__box__nav__li'>Другое</li>
-        //                         </ul>
-        //                     </nav>   
-        //                 </div> 
-                    
-        //                 <div className="header__box__bottom__btn">
-        //                     <img className='header__box__bottom__btn__bag' src={headerShopBag} alt="Корзина" />
-        //                     <p className='header__box__bottom__btn__r'>0 ₽</p>
-        //                 </div>
-        //             </motion.div>
-        //         </div>
-        //     )}
-        // </header>
     );
 };
 
